d14a73fdef27946068d096d38a505a874243edbc
Jonathan Garay <jonathan@devmask.net>
2012-02-20 21:25:10 -0600

    my shiny app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyShinyApp::Application.config.secret_token = '835007a677bdedc2da6b63c86f293ac28ad3c8e2ed95eb7093365a3d5f7a6e7acce71e1fdcd2317da622740fd2e1c8f4c5f471f0e6e13ccfcedcf05380ff3d0b'
